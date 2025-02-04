d67f613aae677f2927cc679e00e4fa956126a6fb
Eren <86615310+erent8@users.noreply.github.com>
2025-02-05 00:59:24 +0300

    Add files via upload

---
+++ WeatherApp/js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiKey: 'API_Key', // OpenWeatherMap API anahtarınızı buraya ekleyin
+    baseUrl: 'https://api.openweathermap.org/data/2.5',
+    units: 'metric',
+    lang: 'tr'
+}; 

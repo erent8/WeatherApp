d12d4edbe6f3d05805a08f067571a5722dd2bd0d
Eren <86615310+erent8@users.noreply.github.com>
2025-02-05 15:57:29 +0300

    Delete WeatherApp directory

--- WeatherApp/js/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiKey: 'API_Key', // OpenWeatherMap API anahtarınızı buraya ekleyin
-    baseUrl: 'https://api.openweathermap.org/data/2.5',
-    units: 'metric',
-    lang: 'tr'
-}; 
